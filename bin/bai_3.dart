83ec3c5575c6228dc6e4e7eedfa0c7e69c98ea2b
Virgonb1 <118275437+Virgonb1@users.noreply.github.com>
2022-11-23 17:08:53 +0700

    first commit

---
+++ bin/bai_3.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  String str =
+      "đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...";
+  print("đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...");
+}
---
+++ test/bai_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:bai_3/bai_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
